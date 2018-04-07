486ab9257bd4316e9b65f818eda7cfd38e7f3212
Eric Renart <eric.renart@stonybrook.edu>
2018-04-07 17:11:19 -0400

    add looping clouds background

--- app/src/main/java/com/bitcamp2018/myclimate/view/MainActivity.java
+++ app/src/main/java/com/bitcamp2018/myclimate/view/MainActivity.java
@@ -1,8 +1,20 @@
 package com.bitcamp2018.myclimate.view;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.VideoView;
+
 
 import com.bitcamp2018.myclimate.R;
 import com.bitcamp2018.myclimate.model.DatabaseAdapter;
@@ -11,14 +23,50 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private DatabaseAdapter adapter;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar menuToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(menuToolbar);
 
         adapter = new DatabaseAdapter(this);
         adapter.loadWeatherData();
+
+        // start the VideoView of the clouds
+        VideoView vv = (VideoView) findViewById(R.id.cloudslove);
+        vv.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.cloudslove));
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
+                mediaPlayer.setLooping(true);
+            }
+        });
+        vv.start();
+
         TextView testView = findViewById(R.id.testView);
         testView.setText(adapter.getCurrentWeather());
     }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 }
