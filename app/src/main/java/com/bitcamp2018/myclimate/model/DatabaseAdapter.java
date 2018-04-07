1062ff95eabc159770f20ad90b5e66ae6fdfaa12
Shane Moe <ssoemoe@terpmail.umd.edu>
2018-04-07 10:13:02 -0400

    created database adapter

---
+++ app/src/main/java/com/bitcamp2018/myclimate/model/DatabaseAdapter.java
@@ -0,0 +1,10 @@
+package com.bitcamp2018.myclimate.model;
+
+/**
+ * Created by shane on 4/7/2018.
+ */
+
+public class DatabaseAdapter {
+
+
+}
--- app/src/main/java/com/bitcamp2018/myclimate/view/MainActivity.java
+++ app/src/main/java/com/bitcamp2018/myclimate/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.bitcamp2018.myclimate;
+package com.bitcamp2018.myclimate.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bitcamp2018.myclimate.R;
 
 public class MainActivity extends AppCompatActivity {
 
